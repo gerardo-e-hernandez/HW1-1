407ccae233865f968181328d5f61fc020317ab9b
Gera <gerardo.e.hernandez01@utrgv.edu>
2017-02-06 00:04:24 -0600

    Added some functions
    Addes some functions to test for intersection and calc slopes. Added
    some checking to see if the lines intercept

--- src/IntersectionChk.java
+++ src/IntersectionChk.java
@@ -8,58 +8,89 @@
 
 import java.util.Scanner;
 
+
 public class IntersectionChk {
 
 	public static void main(String[] args) {
 		
-		//x coordinates
-		int x1, x2 ;
-		//y coordinates
-		int y1, y2 ;
-		//slopes 
-		int m1 ,m2 ;
-		
-		//get input
 		Scanner input = new Scanner(System.in);
+		int[] coords = new int[8] ; // 0:=x1 1:=y1 2:=x2 3:=y2
+		double[] slopes ;
+		double[] yInt;
 		
-		System.out.print("Please enter (x1,y1) for line 1: ");
-		x1 = input.nextInt() ;
-		y1 = input.nextInt() ;
+		char again = 'y';
 		
-		System.out.print("Please enter (x2,y2) for line 1: ");
-		x2 = input.nextInt() ;
-		y2 = input.nextInt() ;
-		
-		//calculate slope
-		// correct division by zero
-		m1 = (y2-y1)/(x2-x1);
-		
-		
-		System.out.print("Please enter (x1,y1) for line 2: ");
-		x1 = input.nextInt() ;
-		y1 = input.nextInt() ;
-		
-		System.out.print("Please enter (x2,y2) for line 2: ");
-		x2 = input.nextInt() ;
-		y2 = input.nextInt() ;
-		
-		//calculate slope
-		// correct division by zero
-		m2 = (y2-y1)/(x2-x1);
-		
-		if( m1 == m2)
+		while(again == 'y' || again == 'Y')
 		{
-			System.out.println("0");
+			System.out.print("Input: ");
 			
-		}
-		else if(m1 != m2)
-		{
-			System.out.println("1");
+			for(int i = 0 ; i < coords.length ; i++)
+			{
+				coords[i] = input.nextInt();
+				
+			}
 			
+			//calculate slopes
+			slopes = calcSlope(coords);
+			//calculate y-intercepts
+			yInt = calcYInt(coords, slopes);
+			
+			if(slopes[0] == slopes[1])
+			{
+				if(yInt[0] == yInt[1])
+				{
+					System.out.println("1");
+										
+				}
+				else
+				{
+					System.out.println("0");
+										
+				}
+			}
+			else if(slopes[0] != slopes[1])
+			{
+				
+				
+			}
+				
+			System.out.print("Do you want to test again? ");
+			again = input.next().charAt(0);
+			if(again != 'y' || again != 'Y')
+			{
+				System.out.println("Thank you");
+			}
+			
+					
 		}
 		
 		input.close();
-
+	}
+	
+	//calculate slope of the give points
+	private static double[] calcSlope(int[] input)
+	{
+		
+		double[] m  = new double[input.length /4] ;
+		
+		for(int i = 0 ; i < input.length ; i+=4)
+		{
+			m[i%4] = ((input[i+3]-input[i+1])/(input[i+2]-input[i]));	
+		}
+		
+		return m ;
+		
+	}
+	
+	//calculate the b component from y = mx + b
+	private static double[] calcYInt(int[] coords, double[] slopes)
+	{
+		double[] intercepts = new double[2];
+		
+		intercepts[0] = coords[1] - slopes[0]*coords[0] ;
+		intercepts[1] = coords[5] - slopes[1]*coords[4] ;
+		
+		return intercepts ;
 	}
 
 }
