51eae5ed233fc8aeddda0fe139fa823743257d91
Gera <gerardo.e.hernandez01@utrgv.edu>
2017-02-07 00:49:08 -0600

    It checks for intersections

--- src/IntersectionChk.java
+++ src/IntersectionChk.java
@@ -2,7 +2,7 @@
  * Gerardo Hernandez
  * SID# 20217586
  * HW1-1
- * January 30, 2017
+ * Due Date 02/07/2017
  *
  */
 
@@ -14,9 +14,9 @@ public class IntersectionChk {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
-		int[] coords = new int[8] ; // 0:=x1 1:=y1 2:=x2 3:=y2
-		double[] slopes ;
-		double[] yInt;
+		int[][] coords = new int[4][2] ; 
+		double[] m ;
+		double[] b;
 		
 		char again = 'y';
 		
@@ -26,37 +26,49 @@ public class IntersectionChk {
 			
 			for(int i = 0 ; i < coords.length ; i++)
 			{
-				coords[i] = input.nextInt();
-				
+				for(int j = 0 ; j < 2 ; j++)
+				{
+					coords[i][j] = input.nextInt();
+				}
 			}
 			
-			//calculate slopes
-			slopes = calcSlope(coords);
-			//calculate y-intercepts
-			yInt = calcYInt(coords, slopes);
 			
-			if(slopes[0] == slopes[1])
+			//calculate slopes
+			m = calcSlope(coords);
+			//calculate y-intercepts
+			b = calcB(coords, m);
+
+
+			//if slopes are the same
+			if(m[0] == m[1])
 			{
-				if(yInt[0] == yInt[1])
+				//then lines never touch 
+				//if y intercepts are not the same
+				if(b[0] != b[1])
 				{
+					//Print a zero if lines never touch
+					System.out.println("0");
+					
+				}
+				else if(b[0] == b[1])//if lines never touch
+				{
+					//but y intercept is the same 
 					System.out.println("1");
 										
 				}
-				else
-				{
-					System.out.println("0");
-										
-				}
+				
 			}
-			else if(slopes[0] != slopes[1])
+			else if(m[0] != m[1])
 			{
-				
-				
+				//then line should intercept			
+				System.out.println("1");
+							
 			}
 				
 			System.out.print("Do you want to test again? ");
 			again = input.next().charAt(0);
-			if(again != 'y' || again != 'Y')
+			
+			if(again == 'n' || again == 'N')
 			{
 				System.out.println("Thank you");
 			}
@@ -68,29 +80,28 @@ public class IntersectionChk {
 	}
 	
 	//calculate slope of the give points
-	private static double[] calcSlope(int[] input)
+	private static double[] calcSlope(int[][] input)
 	{
 		
-		double[] m  = new double[input.length /4] ;
-		
-		for(int i = 0 ; i < input.length ; i+=4)
-		{
-			m[i%4] = ((input[i+3]-input[i+1])/(input[i+2]-input[i]));	
-		}
+		double[] m  = new double[2] ;
+
+		m[0] = ((input[1][1]-input[0][1])/(input[1][0]-input[0][0]));	
+		m[1] = ((input[3][1]-input[2][1])/(input[3][0]-input[2][0]));	
 		
 		return m ;
 		
 	}
 	
 	//calculate the b component from y = mx + b
-	private static double[] calcYInt(int[] coords, double[] slopes)
+	private static double[] calcB(int[][] coords, double[] slopes)
 	{
 		double[] intercepts = new double[2];
 		
-		intercepts[0] = coords[1] - slopes[0]*coords[0] ;
-		intercepts[1] = coords[5] - slopes[1]*coords[4] ;
+		intercepts[0] = coords[1][0] - slopes[0]*coords[0][0] ;
+		intercepts[1] = coords[2][1] - slopes[1]*coords[2][0] ;
 		
 		return intercepts ;
 	}
+	
 
 }
